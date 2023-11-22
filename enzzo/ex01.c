63498fc8c8f280404685c97923ab56f2c9cc8738
EnzzoBez <151688241+EnzzoBez@users.noreply.github.com>
2023-11-22 00:52:52 +0000

    Segundo Exercicio

---
+++ enzzo/ex01.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int valorA,valorB,valorC,valorD,soma, media;
+    printf("Digite o valorA:");
+    scanf("%i",&valorA);
+    printf("Digite o valorB:");
+    scanf("%i",&valorB);
+    printf("Digite o valorC:");
+    scanf("%i",&valorC);
+    printf("Digite o valorD:");
+    scanf("%i",&valorD);
+    soma = valorA + valorB + valorC +valorD;
+    media = soma/4;
+    
+    
+    
+    
+    printf("\n+: %i",media);
+   
+
+
+    return 0;
+
+   
+
+
+    return 0;
+}
