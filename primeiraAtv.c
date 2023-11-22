02c087439e9fe8febaae038f9f6bcce5dfa3a2a1
Wesw3s <151688352+Wesw3s@users.noreply.github.com>
2023-11-22 20:54:31 -0300

    Create primeiraAtv.c

---
+++ primeiraAtv.c
@@ -0,0 +1,10 @@
+int main() {
+    int numero;
+    printf ("Digite um número: ");
+    scanf ("%i",&numero);
+    printf ("O número antecessor a esse é: %i \n",(numero-1));
+    printf ("O número sucessor a esse é: %i",(numero+1));
+    
+    
+    return 0;
+}
