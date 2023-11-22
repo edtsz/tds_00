5f8ac34770089ab917b70830249cf15200041fbe
bertua <151688160+bertua@users.noreply.github.com>
2023-11-22 00:00:58 +0000

    prmeiro exercisio de logiica

---
+++ lucas/ex00.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main() {
+    int num1, num2, resultado;
+    printf("digite o primeiro numero: ");
+    scanf("%i", &num1);
+    printf("digite o segundo numero: ");
+    scanf("%i", &num2);
+    resultado = num1 + num2;
+    printf("o resultado é: %i/n", resultado);
+    
+
+    return 0;
+}
