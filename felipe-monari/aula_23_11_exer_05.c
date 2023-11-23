4b2b0b4368c9fb62363b59903fa9c5ba1c2abe89
Monari14 <133614580+Monari14@users.noreply.github.com>
2023-11-23 19:40:57 -0300

    Update aula_23_11_exer_05.c

--- felipe-monari/aula_23_11_exer_05.c
+++ felipe-monari/aula_23_11_exer_05.c
@@ -1,19 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    float n1, n2, n3;
-    printf("1° número: ");
-    scanf("%f",&n1);
-    printf("2° número: ");
-    scanf("%f",&n2);
-    printf("3° número: ");
-    scanf("%f",&n3);
-    if(n1 > n2 && n1 > n3){
-        printf("O 1° número é o maior");
-    }else if(n2 > n1 && n2 > n3){
-        printf("O 2° número é o maior");
-    }else if(n3 > n1 && n3 > n1){
-        printf("O 3° número é o maior");
-    }
+    float precoProduto, precoPromocional;
+    printf("Preço do produto: ");
+    scanf("%f",&precoProduto);
+    precoPromocional = precoProduto-(precoProduto * 0.05);
+    printf("O preço promocional é: %f",precoPromocional);
     return 0;
 }
