14a1f99b19208f434f20bce54d019bc2c1945c15
Monari14 <133614580+Monari14@users.noreply.github.com>
2023-11-23 19:22:36 -0300

    Delete wesley-m directory

--- wesley-m/soma.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-void main (){
-    int numero1, numero2, soma;
-    numero1=10;
-    numero2=45;
-    soma=numero1+numero2;
-    printf ("A soma entre os dois números é %i \n",soma);
-}
