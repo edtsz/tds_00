e3962de221c6fdfbe66475c36bdc4046d6c94535
Wesw3s <151688352+Wesw3s@users.noreply.github.com>
2023-11-24 19:22:43 -0300

    Delete aula_23_11_exer_05.c

--- aula_23_11_exer_05.c
+++
@@ -1,9 +0,0 @@
-int main() {
-   float desconto, preco, precoDesconto;
-   printf ("Digite o preço do produto: ");
-   scanf ("%f",&preco);
-   desconto=preco*0.05;
-   precoDesconto=preco-desconto;
-   printf ("O valor do seu produto com desconto é: %f",precoDesconto);
-    return 0;
-}
