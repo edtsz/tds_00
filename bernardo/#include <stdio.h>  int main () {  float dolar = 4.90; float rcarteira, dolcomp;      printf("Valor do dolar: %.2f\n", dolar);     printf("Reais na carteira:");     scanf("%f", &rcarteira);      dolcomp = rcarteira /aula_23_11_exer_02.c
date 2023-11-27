354169bf1c4f7edd162e755d77196269797d1472
BeSimonetti <151688438+BeSimonetti@users.noreply.github.com>
2023-11-26 22:11:54 -0300

    Create aula_23_11_exer_02.c

---
+++ bernardo/#include <stdio.h>  int main () {  float dolar = 4.90; float rcarteira, dolcomp;      printf("Valor do dolar: %.2f\n", dolar);     printf("Reais na carteira:");     scanf("%f", &rcarteira);      dolcomp = rcarteira /aula_23_11_exer_02.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main () {
+
+float dolar = 4.90;
+float rcarteira, dolcomp;
+
+    printf("Valor do dolar: %.2f\n", dolar);
+    printf("Reais na carteira:");
+    scanf("%f", &rcarteira);
+
+    dolcomp = rcarteira / dolar;
+
+    printf("Dolares comprados: %.2f\n", dolcomp);
+
+}
