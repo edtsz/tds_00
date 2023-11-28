292faca1b40308cd7a93521ebbb7b53aed70ea33
Guilherme-Thunder <151688223+Guilherme-Thunder@users.noreply.github.com>
2023-11-28 23:24:44 +0000

    commit de correcao 2

---
+++ guilherme/ex00.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int valorA = 10;
+    int valorB = 11;
+    int valfin = valorA + valorB;
+
+    printf("%d", valfin);
+};
---
+++ guilherme/ex01.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int valorA, valorB, valfin, valfinS, valfinM, valfinV, valfinD;
+
+    valorA = 50;
+    valorB = 2;
+
+    valfinS = valorA + valorB;
+    valfinM = valorA - valorB;
+    valfinV = valorA * valorB;
+    valfinD = valorA / valorB;
+
+    printf("%d\n", valfinS);
+    printf("%d\n", valfinM);
+    printf("%d\n", valfinV);
