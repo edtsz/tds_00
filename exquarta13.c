cd532fd99cd3c6ae11c82cbff2e0ca4855d77947
BeSimonetti <151688438+BeSimonetti@users.noreply.github.com>
2023-11-29 21:43:16 -0300

    att2 exquarta

---
+++ exquarta13.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+int main() {
+
+    float comprimento, largura, aterreno;
+    
+    printf("Comprimento do terreto: ");
+    scanf("%f", &comprimento);
+    printf("Largura do terreno: ");
+    scanf("%f", &largura);
+    
+    aterreno = comprimento*largura;
+    
+    printf("Área do terreno: %fm²", aterreno);
+    
+    if(aterreno){
+        
+    }
+    
+    
+    return 0;
+}
